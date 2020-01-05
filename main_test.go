e0829a48edb83b18ff22a7ec2ce46dc405df3943
SUHROB <SRahmonov>
2020-01-05 12:20:12 +0500

    tests: test for nps added

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func Test_cashback(t *testing.T) {
+	tests := []struct {
+		name   string
+		amount int
+		want   int
+	}{
+		// TODO: Add test cases.
+		{"Have cashback", 5000, 250},
+		{"No cashback", 1000, 0},
+		{"Cashback on bound", 3000, 150},
+	}
+	for _, test := range tests {
+		got := cashback(test.amount)
+		if got != cashback(test.want) {
+			t.Error("For cashback test:", test.name, "got:", test.name, "want:", test.want)
+		}
+	}
+}
